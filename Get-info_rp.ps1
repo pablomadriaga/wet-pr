f3d4d63ac6b286072711897fa05f236ce942757d
pablomadriaga <75373441+pablomadriaga@users.noreply.github.com>
2021-02-26 13:14:15 -0300

    v1

---
+++ Get-info_rp.ps1
@@ -0,0 +1,30 @@
+﻿$vc = Read-Host -Prompt "Incregre el Vcenter"
+
+
+#Conectarse al Vcenter
+try {
+    Write-Host "Conectando con vCenter $vcenter , por favor espere ..." -ForegroundColor Cyan
+    Connect-VIServer $vc -ErrorAction Stop | Out-Null
+}#Fin del Try
+catch {
+    Write-Host "No se puede conectar con el servidor" -ForegroundColor Yellow
+    Break
+}#Fin del Catch
+
+
+$clusterName = Read-Host -Prompt "Incregre el CLuster"
+
+$cluster = Get-Cluster -Name $clusterName
+Get-ResourcePool -Location $cluster |
+Select Name,@{N="Parent";E={
+  $path = $_.Parent.Name,$_.Name -join '/'
+  $parent = $_.Parent
+  while($parent -isnot [VMware.VimAutomation.ViCore.Impl.V1.Inventory.ClusterIMpl]){
+    $path = $parent.Parent.Name,$path -join '/'
+    $parent = $parent.Parent
+  }
+  $path}},ParentId,CpuSharesLevel,NumCpuShares,CpuReservationMHz,CpuExpandableReservation,CpuLimitMHz,MemSharesLevel,NumMemShares,MemReservationMB,MemReservationGB,MemExpandableReservation,MemLimitMB,MemLimitGB,CustomFields,ExtensionData,Id,Uid |
+Export-Csv rp-export.csv -NoTypeInformation -UseCulture
+
+
+Disconnect-VIServer * -Force
---
+++ Set-info_rp.ps1
@@ -0,0 +1,35 @@
+﻿$vc = Read-Host -Prompt "Incregre el Vcenter"
+
+#Conectarse al Vcenter
+try {
+    Write-Host "Conectando con vCenter $vcenter , por favor espere ..." -ForegroundColor Cyan
+    Connect-VIServer $vc -ErrorAction Stop | Out-Null
+}#Fin del Try
+catch {
+    Write-Host "No se puede conectar con el servidor" -ForegroundColor Yellow
+    Break
+}#Fin del Catch
+
+
+
+foreach($row in (Import-Csv rp-export.csv -UseCulture)){
+  $clusterName,$pools = $row.Parent.Split('/')
+  Try {
+    $skip = $false
+    $location = Get-Cluster -Name $clusterName -ErrorAction Stop
+  }
+  Catch {
+    "Cluster $clusterName not found"
+    $skip = $true
+  }
+  if(!$skip){
+    foreach($rp in $pools){
+      Try {
+        $location = Get-ResourcePool -Name $rp -Location $location -ErrorAction Stop
+      }
+      Catch {
+        $location = New-ResourcePool -Name $rp -Location $Location -NumCpuShares $row.NumCpuShares -CpuReservationMHz $row.CpuReservationMHz -CpuExpandableReservation ([System.Convert]::ToBoolean($row.CpuExpandableReservation)) -CpuLimitMHz $row.CpuLimitMHz
+      }
+    }
+  }
+}
